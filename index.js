24295b753c2d89b0e0d71d1aa73e51b9f8e4203f
Othman Mashaab <red.fantastic@hotmail.com>
2018-06-08 01:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function init() {
   //put any page initialization/handlebars initialization here
   var target = document.getElementById('recipe-form-template').innerHTML;
+  handlebars.compile()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
